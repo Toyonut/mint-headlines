8358eee3f9a763069046e86385d03feb16d223e6
Paul Milbank <paulmilbank@gmail.com>
2018-06-08 22:45:00 +1200

    initial commit

---
+++ MintScrape.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace mint_headlines
+{
+    class MintScrape
+    {
+        public void GetFeedXml(Uri feedUri)
+        {
+            var xmlFeedData = GetXmlFeedData(feedUri);
+
+            XmlDocument rssXmlDoc = new XmlDocument();
+
+            try
+            {
+                rssXmlDoc.LoadXml(xmlFeedData);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine($"Error loading XML: {err}");
+            }
+
+            if (rssXmlDoc.HasChildNodes)
+            {
+                var rssXmlDocNodes = rssXmlDoc.SelectNodes("/rss/channel/item");
+
+                for (var i = 0; i < rssXmlDocNodes.Count; i++)
+                {
+                    Console.WriteLine(rssXmlDocNodes.Item(i).SelectSingleNode("title").InnerXml);
+                    Console.WriteLine(rssXmlDocNodes.Item(i).SelectSingleNode("link").InnerXml);
+                    Console.WriteLine();
+
+                    var cdataComment = extractCData(rssXmlDocNodes.Item(i).SelectSingleNode("description").OuterXml);
+                    Console.WriteLine(cdataComment);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private string GetXmlFeedData(Uri feedUri)
+        {
+            string feedXml = "";
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders
+                    .Accept
+                    .Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+
+                    var request = new HttpRequestMessage(HttpMethod.Get, feedUri);
+
+                    feedXml = httpClient.SendAsync(request).Result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            return feedXml;
+        }
+
+        public string extractCData(string cdataSection)
+        {
+            XDocument message = new XDocument(XDocument.Parse(cdataSection));
+
+            XCData cdata = message.DescendantNodes().OfType<XCData>().ToList()[0];
+            string cDataContent = cdata.Value;
+
+            // Some character normalization - yes this is a dirty hack...
+            cDataContent = cDataContent.Replace(" [&#8230;]", "...");
+            cDataContent = cDataContent.Replace("&#8217;", "'");
+            cDataContent = cDataContent.Replace("&#8220;", "\"");
+            cDataContent = cDataContent.Replace("&#8221;", "\"");
+            cDataContent = cDataContent.Replace("&#8211;", "-");
+
+            return cDataContent;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mint_headlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mintScraper = new MintScrape();
+
+            Uri mintFeedUri = new Uri("https://blog.linuxmint.com/?feed=rss2");
+
+            string value = $"Fetching feed data from {mintFeedUri}.\n";
+            Console.WriteLine(value);
+
+            mintScraper.GetFeedXml(mintFeedUri);
+        }
+    }
+}
